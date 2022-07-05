b1734b89f38cfaa05f49d0dd45b5fe918c8504ce
Djarrah <vexeniolupin@hotmail.it>
2022-07-05 18:52:08 +0200

    Contabilità
    + Class
    + Aspx page and c# behind

---
+++ App_Code/Contabilita.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrizione di riepilogo per Contabilita
+/// </summary>
+public class Contabilita
+{
+    int anno;
+
+    public Contabilita(int anno) { this.anno = anno; }
+
+    public decimal SpesePerAnno()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "tabSpese_SumForYear";
+        cmd.Parameters.AddWithValue("@anno", anno);
+
+        Connessione conn = new Connessione();
+        DataTable dt = conn.EseguiSPSelect(cmd);
+
+        return decimal.Parse(dt.Rows[0]["Spese"].ToString());
+    }
+
+    public decimal FatturatoPerAnno()
+    {
+        decimal fatturatoAnnuo = 0;
+        for(int i=1; i<13; i++) { fatturatoAnnuo += FatturatoPerMese(i); }
+
+        return fatturatoAnnuo;
+    }
+
+    public decimal RicaviPerAnno() { return FatturatoPerAnno() - SpesePerAnno(); }
+
+    public decimal FatturatoPerMese(int mese)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "tabContratti_MonthlySumByYear";
+        cmd.Parameters.AddWithValue("@anno", anno);
+        cmd.Parameters.AddWithValue("@mese", mese);
+
+        Connessione conn = new Connessione();
+        DataTable dt = conn.EseguiSPSelect(cmd);
+
+        return decimal.Parse(dt.Rows[0]["Fatturato"].ToString());
+    }
+}
---
+++ Contabilita.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            for (int i = DateTime.Now.Year; i >= DateTime.Now.Year - 2; i--)
+            {
+                ddlAnno.Items.Add(i.ToString());
+            }
+        }
+    }
+
+    protected void btnCalcola_Click(object sender, EventArgs e)
+    {
+        int anno = int.Parse(ddlAnno.SelectedValue);
+        Contabilita c = new Contabilita(anno);
+        DateTimeFormatInfo dtf = CultureInfo.CurrentCulture.DateTimeFormat;
+
+        lblSpese.Text = "€ " + c.SpesePerAnno();
+        lblFatturato.Text = "€ " + c.FatturatoPerAnno();
+        lblRicavi.Text= "€ " + c.RicaviPerAnno();
+
+        string tabellaMesi = "";
+        for (int i = 1; i < 13; i++)
+        {
+            string nomeMese = dtf.GetMonthName(i);
+            nomeMese = char.ToUpper(nomeMese[0]) + nomeMese.Substring(1);
+
+            decimal fatturatoMese = c.FatturatoPerMese(i);
+
+            tabellaMesi += $"<tr><td>{nomeMese}</td><td>{fatturatoMese}</td></tr>";
+        }
+
+        litTabellaMesi.Text = tabellaMesi;
+    }
+}
